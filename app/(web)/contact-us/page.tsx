6bb678808f42cb357eaf79554d40d348d64621cc
Fathima Dilsha <fathima-dilsha@gmail.com>
2026-09-24 11:26:08 +0530

    bb

---
+++ app/(web)/contact-us/page.tsx
@@ -0,0 +1,12 @@
+import ContactInfoSection from "@/app/component/contact/ContactInfoSection";
+
+export default function AboutPage() {
+    return (
+        <main>
+
+            <ContactInfoSection />
+
+
+        </main>
+    );
+}
